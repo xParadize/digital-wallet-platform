606d7c633d24b0689fc585ca6800f1ec500f4eaf
Stepan Romanov <stepanr17@mail.ru>
2025-10-29 10:50:16 +0300

    fix(api-gateway): replace deprecated gateway dependency with webflux version
    Replaced spring-cloud-starter-gateway with spring-cloud-starter-gateway-server-webflux
    to fix startup failure caused by deprecated dependency.
    Updated application.yml configuration keys to use
    spring.cloud.gateway.server.webflux.* properties.

--- api-gateway/src/main/java/com/wallet/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/wallet/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,9 @@ package com.wallet.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
